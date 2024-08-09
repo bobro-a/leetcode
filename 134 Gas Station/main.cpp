6b2c58da5aebdf12a6d396584f9b50fd83a66ebf
bobro <m3_ba@mail.ru>
2024-08-09 15:09:22 +0300

    не решила

---
+++ 134 Gas Station/main.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <set>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+
+        for (int i=0;i<gas.size();++i)
+        {
+
+        }
+    }
+};
+
+
+int main()
+{
+    vector<int> gas = {1,2,3,4,5};
+    vector<int> cost={3,4,5,1,2};
+    Solution a;
+    int rez = a.canCompleteCircuit(gas,cost);
+    return 0;
+}
