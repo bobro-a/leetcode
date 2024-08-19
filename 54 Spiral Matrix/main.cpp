11a493317d5197781a7edc68572b9d2771df929c
bobro <m3_ba@mail.ru>
2024-08-19 13:31:39 +0300

    пока не получилось

---
+++ 54 Spiral Matrix/main.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <map>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+
+    }
+};
+
+
+int main()
+{
+    vector<vector<int>> matrix = {{1,2,3},{4,5,6},{7,8,9}};
+    Solution a;
+    a.spiralOrder(matrix);
+    return 0;
+}
